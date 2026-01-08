58dcdab7053c6c4e7586bfe105bb322adf3d5e71
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-01-08 08:54:10 -0500

    Segundo Commit, Arreglado Errores y mejorando interactividad

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type FormEvent } from 'react'
 import './Card.css'
-import Inputs from './Inputs/Inputs'
 
 const Card = () => {
 
@@ -11,10 +10,13 @@ const Card = () => {
     const [weatherMain, setWeatherMain] = useState('')
     const [weatherDescription, setWeatherDescription] = useState('')
 
-    useEffect(() => {
-        const fetchData = async () => {
+    const showWeather = async (event: FormEvent) => {
+        event.preventDefault()
+        if (!city) return
+        try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d19893e0c02086e19e8477f5ba04e23&units=metric&lang=es`)
-            if (!response.ok) throw new Error("Error: " + response.status)
+            if (!response.ok) throw new Error("Error http: " + response.status)
+
             const data = await response.json()
 
             setTemp(data.main.temp)
@@ -22,19 +24,37 @@ const Card = () => {
             setCodeWeather(data.weather[0].icon)
             setWeatherDescription(data.weather[0].description)
             setHumidity(data.main.humidity)
+        } catch (error) {
 
         }
-        fetchData()
-    }, [])
 
-
+    }
 
     return (
         <div className={`bg-neutral-700 justify-center p-3 items-center flex flex-col w-[25dvw] h-[55dvh] rounded-2xl text-white`}>
-            <Inputs city={setCity}></Inputs>
+            <div>
+                <form className="flex flex-col " onSubmit={showWeather}>
+                    <input
+                        placeholder="Ciudad..."
+                        type="text"
+                        value={city}
+                        className="bg-white my-3 w-[20dvw] h-[2dvw] rounded-2xl text-black p-2 placeholder:text-neutral-500"
+                        onChange={(event) => {
+                            setCity(event.target.value)
+                            if (event.target.value == '') {
+                                setCodeWeather('')
+                                setHumidity('')
+                                setTemp('')
+                                setWeatherDescription('')
+                                setWeatherMain('')
+                            }
+                        }}
+                    />
+                </form>
+            </div>
             {codeWeather && <img className='bg-white rounded-2xl' src={`https://openweathermap.org/img/wn/${codeWeather}@2x.png`} alt="Clima Icon" />}
             <h2 className='font-bold text-2xl'>{city && `Ciudad: ${city}`}</h2>
-            <h1 className='font-bold text-5xl'>{temp == '' ? '-' : `${temp}°C`}</h1>
+            <h1 className='font-bold text-5xl'>{temp && `${temp}°C`}</h1>
             <h2 className='font-bold text-2xl'>{weatherMain && `Clima: ${weatherMain}`}</h2>
             <h2 className='font-bold text-2xl'>{weatherDescription && `Description: ${weatherDescription}`}</h2>
             <h2 className='font-bold text-2xl'>{humidity && `Humedad: ${humidity}%`}</h2>
